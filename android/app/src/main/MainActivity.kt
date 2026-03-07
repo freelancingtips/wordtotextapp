1322c49d90d5fe579b0d1640ff924959aec06532
freelancingtips <vesaljan66@gmail.com>
2026-03-07 12:48:35 +0500

    Add WebView to MainActivity for loading URL

--- android/app/src/main/MainActivity.kt
+++ android/app/src/main/MainActivity.kt
@@ -1,1 +1,21 @@
+package com.example.wordtotext
 
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val webView: WebView = findViewById(R.id.webView)
+
+        webView.settings.javaScriptEnabled = true
+        webView.webViewClient = WebViewClient()
+
+        webView.loadUrl("https://freelancingtips.github.io/wordtotextapp/")
+    }
+}
